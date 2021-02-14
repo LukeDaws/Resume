b16017c7c137f39054111837dd62b6634e4705da
LukeDaws <Luke.Daws@gmail.com>
2021-02-14 20:11:48 +1100

    included STATIC_ROOT

--- webresume/settings.py
+++ webresume/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = Path(__file__).resolve().parent.joinpath(BASE_DIR, "static")
 STATICFILES_DIRS = [Path(__file__).resolve().parent.joinpath(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
